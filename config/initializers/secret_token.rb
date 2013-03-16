650814b89674a1980ba3dc41fd8060de92f804eb
Sinelnikov Alexander <sinelniko@gmail.com>
2013-03-16 20:04:43 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lolme::Application.config.secret_token = 'c3b00a91bcb34cf52801c169863c8e76b8ee01bdb6a29df2a2edc0df5eb027b07a6a1b531a84d20a60dc8821fc0f4648e9a10af4e520e62584da3061df412918'
