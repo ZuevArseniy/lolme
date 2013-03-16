e194e39a2e4be588685d0b5afa094476582456b1
Sinelnikov Alexander <sinelniko@gmail.com>
2013-03-16 23:51:09 +0200

    parser returns array of hashed with specified keys

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,14 +2,15 @@ class Parser
   require 'open-uri'
 
   def self.bash_im
-      page = Nokogiri::HTML(open("http://bash.im/"))       #  Nokogiri::HTML::Document
-      quote_array = page.css('div.quote')                  #array of texts
+      bash_array = []
+      page = Nokogiri::HTML(open("http://bash.im/"))
+      quote_array = page.css('div.quote')
       quote_array.each do |quote|
         unless quote.css('div.text').empty? && quote.css('div.actions.a').empty?
-        quote.css('div.text').text                        #text by itself
-        quote.css('div.actions a').last.text[1..-1]              #id of text
-        Time.parse(quote.css('div.actions span').last.text)  #created_at on bash
+          quote = {extermal_id: quote.css('div.actions a').last.text[1..-1], content: quote.css('div.text').text, external_time: Time.parse(quote.css('div.actions span').last.text)    }
+          bash_array.push(quote)
         end
       end
+    bash_array
   end
 end
