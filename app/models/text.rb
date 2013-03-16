5d057d43a8db24cb0df7683fd1f4b7f2bd8b013b
arseniy <arseniy@zuev.by>
2013-03-17 01:21:49 +0300

    Validate uniqueness before save

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,4 +1,10 @@
 class Text < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :source
+
+  before_save :validate_uniqueness
+
+  def validate_uniqueness
+  	self.class.where(external_id:self.external_id, source_id:self.source_id).empty?
+  end
 end
