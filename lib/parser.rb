dca75d6af22d4b93d02213c2cc47b3f4936a9297
Sinelnikov Alexander <sinelniko@gmail.com>
2013-03-16 23:52:52 +0200

    Fixed typo in key name

--- lib/parser.rb
+++ lib/parser.rb
@@ -7,7 +7,7 @@ class Parser
       quote_array = page.css('div.quote')
       quote_array.each do |quote|
         unless quote.css('div.text').empty? && quote.css('div.actions.a').empty?
-          quote = {extermal_id: quote.css('div.actions a').last.text[1..-1], content: quote.css('div.text').text, external_time: Time.parse(quote.css('div.actions span').last.text)    }
+          quote = {external_id: quote.css('div.actions a').last.text[1..-1], content: quote.css('div.text').text, external_time: Time.parse(quote.css('div.actions span').last.text)    }
           bash_array.push(quote)
         end
       end
